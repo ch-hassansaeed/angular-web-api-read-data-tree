b3b93bea0fdada786d1d266e97aee3498777ae04
ch-hassansaeed <ch.hassansaeed@gmail.com>
2020-11-11 19:12:35 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# angular-web-api-read-data-tree
+# angular-8-basic-authentication-example
+
+Angular 8 - Basic HTTP Authentication Example with Angular CLI
+
+For a demo and further details see https://jasonwatmore.com/post/2019/06/26/angular-8-basic-http-authentication-tutorial-example
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService } from '@app/_services';
+
+import { HttpClient } from '@angular/common/http';
+
+
+
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    users: User[];
+    category_items:any =[];
+
+
+    constructor(private http: HttpClient) { 
+        var url="http://208.109.13.111:9090/api/Category";
+        //get json from API url
+        this.http.get(url).subscribe(
+            data => {
+                      console.log("I CAN SEE JSON DATA HERE: ", data);
+                      this.category_items = data["result"];
+                     }
+        ); 
+    }
+
+    parent_click(el){
+        //alert(el);
+        // alert("register click event for categories");
+                            //tree item click logic
+                            //var toggler = document.getElementsByClassName("caret");
+                            //var i;
+
+                           // for (i = 0; i < toggler.length; i++) {
+                            //toggler[i].addEventListener("click", function() {
+                                el.parentElement.querySelector(".nested").classList.toggle("active");
+                                el.classList.toggle("caret-down");
+                            //});
+                           // }
+                            //this.myFunc(this);
+      }
+
+     
+
+    ngOnInit() {
+        this.loading = true;
+        //this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+          //  this.users = users;
+
+
+        
+
+
+
+
+    }
+}
